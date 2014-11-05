200278a7b5bdf0c002aa7463940f1d07d736d499
Andrew Khalymon <akhalymon@hortonworks.com>
2014-11-05 14:39:35 +0200

    Added stopping iptables for centOS 6.4

--- centos6.4/bootstrap.sh
+++ centos6.4/bootstrap.sh
@@ -7,6 +7,9 @@ service ntpd start
 service iptables stop
 mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
 
+#Again, stopping iptables
+/etc/init.d/iptables stop
+
 # Increasing swap space
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=3072k
 sudo mkswap /swapfile
