1239440cf6164c6c08aee97a3576e737a4baba69
Yusaku Sako <yusaku@hortonworks.com>
2015-03-19 10:00:04 -0700

    Add vagrant image for debian 7.

--- ubuntu12.4/snapshot.sh
+++ ubuntu12.4/snapshot.sh
@@ -30,6 +30,9 @@ set_vm_prefix(){
 	ubuntu12.4)
 		vm_prefix=u12
 	;;
+        debian7.6)
+                vm_prefix=d76
+        ;;
 	*)
 		echo "Unrecognized working folder! $0 needs to be placed into on of the following: "
 		echo "centos 6.5 centos6.4 centos5.8. centos5.9 suse11 ubuntu12.4"
