bc4f9b4f561ae0755f8d889e5a20717eb217262c
Yusaku Sako <yusaku@hortonworks.com>
2015-12-15 00:22:14 -0800

    Update dev-bootstrap.sh to correctly install build dependencies

--- centos6.4/dev-bootstrap.sh
+++ centos6.4/dev-bootstrap.sh
@@ -9,16 +9,17 @@ service ntpd start
 yum install git -y
 yum install rpm-build -y
 yum install gcc-c++ -y
-wget http://nodejs.org/dist/v0.10.31/node-v0.10.31-linux-x64.tar.gz
-tar zxvf node-v0.10.31-linux-x64.tar.gz
-mv node-v0.10.31-linux-x64 /usr/share/node
-/usr/share/node/bin/npm install -g brunch
+yum install python-devel -y
+wget http://nodejs.org/dist/v0.10.40/node-v0.10.40-linux-x64.tar.gz
+tar zxvf node-v0.10.40-linux-x64.tar.gz
+mv node-v0.10.40-linux-x64 /usr/share/node
+/usr/share/node/bin/npm install -g brunch@1.7.20
 wget --no-check-certificate http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg
 sh setuptools-0.6c11-py2.6.egg
-wget http://psg.mtu.edu/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
+wget http://apache.cs.utah.edu//maven/binaries/apache-maven-3.0.5-bin.tar.gz
 tar zxvf apache-maven-3.0.5-bin.tar.gz
 mv apache-maven-3.0.5 /usr/share/maven
-yum install java-1.6.0-openjdk-devel -y
+yum install java-1.7.0-openjdk-devel -y
 
 # make maven and node available in PATH
 echo 'pathmunge /usr/share/node/bin' > /etc/profile.d/node.sh
